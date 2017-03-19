a9d01f0310a71fed65d896a13306dcbad6c6fff7
Haku <info@hakanu.net>
2017-03-19 19:01:32 +0000

    Add real search mechanism for real

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,10 +1,11 @@
-// A $( document ).ready() block.
+var _RAW_QUERY = '';
+
 $( document ).ready(function() {
     console.log( "ready!" );
     $('#div-search-results').hide();
     $('#btn-search').click(function(args) {
-      var query = $('#input-query').val();
-      searchJob(query);
+      _RAW_QUERY = $('#input-query').val();
+      GetJobKeys(_RAW_QUERY);
     });
 
     $('#btn-login').click(function(args){
@@ -27,17 +28,25 @@ $( document ).ready(function() {
                   name + ' ' + uni + ' ' + uniDept);
       // TODO(hakanu): Firebase sign up here.
     });
-
-    FBTrials('eleman');
 });
 
-function searchJob(query) {
-  console.log('Searching job for ' + query);
+function searchJob(rawQuery) {
+  console.log('Searching job for ' + rawQuery);
+  var queryWords = rawQuery.split(' ');
+
+  var results = [];
+  for (var i = queryWords.length - 1; i >= 0; i--) {
+    var query = queryWords[i];
+    SearchFb(query);
+    break;
+  }
+
   $('#div-search-results').show();
 }
 
-function FBTrials(query) {
-  console.log('FBTrials');
+function GetJobKeys(query) {
+  console.log('GetJobKeys ' + query);
+  var jobKeys = [];
   var ref = firebase.database().ref('jobs_index/' + query).orderByValue().limitToFirst(20);
   ref.once('value', function(snapshot) {
     snapshot.forEach(function(childSnapshot) {
@@ -45,6 +54,41 @@ function FBTrials(query) {
       var childData = childSnapshot.val();
       console.log('childKey: ' + JSON.stringify(childKey));
       console.log('childData: ' + JSON.stringify(childData));
+      jobKeys.push(childData);
     });
+    GetJobs(jobKeys);
   });
+}
+
+function GetJobs(jobKeys) {
+  console.log('GetJobs ' + jobKeys);
+  var jobs = [];
+  for (var i = jobKeys.length - 1; i >= 0; i--) {
+    var key = jobKeys[i];
+    var ref = firebase.database().ref('jobs').orderByChild('link').equalTo(key).limitToFirst(20);
+    ref.once('value', function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var childKey = childSnapshot.key;
+        var childData = childSnapshot.val();
+        console.log('childKey: ' + JSON.stringify(childKey));
+        console.log('childData: ' + JSON.stringify(childData));
+        jobs.push(childData);
+      });
+      updateUI(jobs);
+    });
+  }
+}
+
+function updateUI(jobs) {
+  console.log('updateUI');
+  $('#h3-search-query').text(_RAW_QUERY);
+  for (var i = jobs.length - 1; i >= 0; i--) {
+    $('#ul-search-results').append(
+        '<li>' +
+        '<h4><a href="' + jobs[i].link + '" target="_blank">' + jobs[i].title + '</a></h4>' + 
+        '<sub>' + jobs[i].city + ' | ' + jobs[i].company + ' | ' + jobs[i].category +
+        ' | ' + jobs[i].date + '</sub>' + 
+        '</li>');
+  }
+  $('#div-search-results').show();
 }
