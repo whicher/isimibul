b970f2bfe703cad87c48dd8d0b1a93ef76271fa1
Haku <info@hakanu.net>
2017-03-19 12:54:56 +0000

    Change the shape of the homepage, add firebase dependencies.

--- static/js/custom.js
+++ static/js/custom.js
@@ -6,9 +6,27 @@ $( document ).ready(function() {
       var query = $('#input-query').val();
       searchJob(query);
     });
+    FBTrials();
 });
 
 function searchJob(query) {
   console.log('Searching job for ' + query);
   $('#div-search-results').show();
+}
+
+function FBTrials() {
+  console.log('FBTrials');
+  var ref = firebase.database().ref('jobs_test/');
+  ref.once('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var childKey = childSnapshot.key;
+      var childData = childSnapshot.val();
+      console.log('childKey: ' + JSON.stringify(childKey));
+      console.log('childData: ' + JSON.stringify(childData));
+    });
+  });
+
+  // ref.orderByChild("height").equalTo(25).on("child_added", function(snapshot) {
+  //   console.log(snapshot.key());
+  // });
 }
