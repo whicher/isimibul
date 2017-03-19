8c6271d591bda5239efca9503e61b992bec6c4f8
Haku <info@hakanu.net>
2017-03-19 21:18:04 +0000

    Add job details page, Add disqus comments.

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,33 +2,71 @@ var _RAW_QUERY = '';
 
 $( document ).ready(function() {
     console.log( "ready!" );
-    $('#div-search-results').hide();
-    $('#btn-search').click(function(args) {
-      _RAW_QUERY = $('#input-query').val();
-      GetJobKeys(_RAW_QUERY);
-    });
 
-    $('#btn-login').click(function(args){
-      console.log('Logging in');
-      var email = $('#input-email').val();
-      var password = $('#input-password').val();
-      console.log('Creds: ' + email + ' ' + password);
-      // TODO(hakanu): Firebase login here.
-    });
+    var currentUrl = window.location.href;
+    if(currentUrl.indexOf('/details?job=') > 0) {
+      console.log('DETAILS PAGE');
+      $('#div-job-details').append('<h1>İş Detayları</h1>');
+      var params = getJsonFromUrl(currentUrl);
+      var jobKey = params.job.substr(params.job.lastIndexOf('_') + 1);
+      console.log('param: ' + jobKey);
 
-    $('#btn-signup').click(function(args){
-      console.log('Signing up');
-      var email = $('#input-signup-email').val();
-      var password = $('#input-signup-password').val();
-      var name = $('#input-signup-name').val();
-      var background = $('#input-signup-background').val();
-      var uni = $('#input-signup-uni').val();
-      var uniDept = $('#input-signup-uni-dept').val();
-      console.log('Creds: ' + email + ' ' + password + ' ' + background + '' + 
-                  name + ' ' + uni + ' ' + uniDept);
-      // TODO(hakanu): Firebase sign up here.
-    });
+      var ref = firebase.database().ref('jobs').orderByChild('hash').equalTo(jobKey).limitToFirst(20);
+      var job = {};
+      ref.once('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          var childKey = childSnapshot.key;
+          var childData = childSnapshot.val();
+          // console.log('childKey: ' + JSON.stringify(childKey));
+          // console.log('childData: ' + JSON.stringify(childData));
+          job = childData;
+        });
+        updateUIDetails(job);
+      });
+
+    } else {
+      console.log('HOMEPAGE');
+      $('#div-search-results').hide();
+
+      $('#btn-search').click(function(args) {
+        console.log('Cleaning up previous search');
+        $('#ul-search-results').children().remove();
+        _RAW_QUERY = $('#input-query').val();
+        GetJobKeys(_RAW_QUERY);
+      });
+
+      $('#btn-login').click(function(args){
+        console.log('Logging in');
+        var email = $('#input-email').val();
+        var password = $('#input-password').val();
+        console.log('Creds: ' + email + ' ' + password);
+        // TODO(hakanu): Firebase login here.
+      });
+
+      $('#btn-signup').click(function(args){
+        console.log('Signing up');
+        var email = $('#input-signup-email').val();
+        var password = $('#input-signup-password').val();
+        var name = $('#input-signup-name').val();
+        var background = $('#input-signup-background').val();
+        var uni = $('#input-signup-uni').val();
+        var uniDept = $('#input-signup-uni-dept').val();
+        console.log('Creds: ' + email + ' ' + password + ' ' + background + '' + 
+                    name + ' ' + uni + ' ' + uniDept);
+        // TODO(hakanu): Firebase sign up here.
+      });
+    }
 });
+
+function getJsonFromUrl() {
+  var query = location.search.substr(1);
+  var result = {};
+  query.split("&").forEach(function(part) {
+    var item = part.split("=");
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+  return result;
+}
 
 function searchJob(rawQuery) {
   console.log('Searching job for ' + rawQuery);
@@ -52,8 +90,8 @@ function GetJobKeys(query) {
     snapshot.forEach(function(childSnapshot) {
       var childKey = childSnapshot.key;
       var childData = childSnapshot.val();
-      console.log('childKey: ' + JSON.stringify(childKey));
-      console.log('childData: ' + JSON.stringify(childData));
+      // console.log('childKey: ' + JSON.stringify(childKey));
+      // console.log('childData: ' + JSON.stringify(childData));
       jobKeys.push(childData);
     });
     GetJobs(jobKeys);
@@ -70,8 +108,8 @@ function GetJobs(jobKeys) {
       snapshot.forEach(function(childSnapshot) {
         var childKey = childSnapshot.key;
         var childData = childSnapshot.val();
-        console.log('childKey: ' + JSON.stringify(childKey));
-        console.log('childData: ' + JSON.stringify(childData));
+        // console.log('childKey: ' + JSON.stringify(childKey));
+        // console.log('childData: ' + JSON.stringify(childData));
         jobs.push(childData);
       });
       updateUI(jobs);
@@ -85,10 +123,23 @@ function updateUI(jobs) {
   for (var i = jobs.length - 1; i >= 0; i--) {
     $('#ul-search-results').append(
         '<li>' +
-        '<h4><a href="' + jobs[i].link + '" target="_blank">' + jobs[i].title + '</a></h4>' + 
+        '<h4><a href="/details?job=' + jobs[i].slug + '_' + jobs[i].hash + '" target="_blank">' + jobs[i].title + '</a></h4>' + 
         '<sub>' + jobs[i].city + ' | ' + jobs[i].company + ' | ' + jobs[i].category +
         ' | ' + jobs[i].date + '</sub>' + 
         '</li>');
   }
   $('#div-search-results').show();
+}
+
+function updateUIDetails(job) {
+  console.log('updateUIDetails');
+  $('#div-job-details').append('<h2>' + job.title + '</h2>');
+  $('#div-job-details').append('<ul>');
+  $('#div-job-details').append('<li>Şehir: <b>' + job.city + '</b></li>');
+  $('#div-job-details').append('<li>Kategori: <b>' + job.category + '</b></li>');
+  $('#div-job-details').append('<li>Gereksinimler & aranan özellikler:<br><b>' + job.description + '</b></li>');
+  $('#div-job-details').append('<li>Şirket: <a href="' + job.company_link + '" target="_blank">' + job.company + '</a></li>');
+  $('#div-job-details').append('<li><img src="' + job.company_img +'"></li>');
+  $('#div-job-details').append('</ul>');
+  $('#div-job-details').append('<p><a href="' + job.link + '" target="_blank">Hemen bu işe başvur</a></h2>');
 }
